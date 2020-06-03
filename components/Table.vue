21ac4c852e5016ae88718bd0f7ed6af4d4e12bb5
acharyasagar <acharyasaagar@gmail.com>
2020-06-03 12:38:32 +0300

    remove confirmed and deaths

--- components/Table.vue
+++ components/Table.vue
@@ -107,7 +107,7 @@ export default {
       page: 1,
       itemsPerPage: 8,
       sortBy: "confirmed",
-      keys: ["Confirmed", "Recovered", "Deaths"],
+      keys: ["Confirmed"],
       items: [],
       loading: true
     };
@@ -127,13 +127,9 @@ export default {
     const items = allDistricts.map(district => {
       const filter = d => d.healthCareDistrict === district;
       const confirmed = data.confirmed.filter(filter);
-      const recovered = data.recovered.filter(filter);
-      const deaths = data.deaths.filter(filter);
       return {
         healthCareDistrict: district || "Others",
-        confirmed: confirmed.length,
-        recovered: recovered.length,
-        deaths: deaths.length
+        confirmed: confirmed.length
       };
     });
     this.items = items;
